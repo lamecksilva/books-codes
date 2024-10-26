695a4cb6cbcf1152ad5e0f5c9239e732fc15a410
Lameck Santos <lameck2@hotmail.com>
2024-10-25 22:09:37 -0300

    Add algorithms

---
+++ entendendo-algoritmos/grafo_manga.py
@@ -0,0 +1,32 @@
+from collections import deque
+
+def pessoa_e_vendedor(nome):
+    return nome[-1] == "m"
+
+grafo = {}
+grafo["voce"] = ["alice", "bob", "claire"]
+grafo["bob"] = ["anuj", "peggy"]
+grafo["alice"] = ["peggy"]
+grafo["claire"] = ["thom", "jonny"]
+grafo["anuj"] = []
+grafo["peggy"] = []
+grafo["thom"] = []
+grafo["jonny"] = []
+
+def pesquisa(nome):
+    fila_de_pesquisa = deque()
+    fila_de_pesquisa += grafo["voce"]
+    verificadas = []
+    
+    while fila_de_pesquisa:
+        pessoa = fila_de_pesquisa.popleft()
+        if not pessoa in verificadas:
+            if pessoa_e_vendedor(pessoa):
+                print(f"{pessoa} é um vendedor de manga!")
+                return True
+            else:
+                fila_de_pesquisa += grafo[pessoa]
+                verificadas.append(pessoa)
+    return False
+
+pesquisa("voce")
