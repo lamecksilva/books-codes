c5a10fe77cff9340dbb6dac43ff2cbd1cf9c60d6
Lameck Santos <lameck2@hotmail.com>
2024-11-09 14:27:21 -0300

    feat: Add scripts

---
+++ introducing-python-modern-computing-in-simple-packages/case.py
@@ -0,0 +1,13 @@
+setup = 'a duck goes into a bar...'
+
+print(setup)
+print(setup.strip('.'))
+print(setup.capitalize())
+print(setup.title())
+print(setup.upper())
+print(setup.lower())
+print(setup.swapcase())
+
+print(setup.center(30))
+print(setup.ljust(30))
+print(setup.rjust(30))
