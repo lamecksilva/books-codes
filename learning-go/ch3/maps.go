fdf3e38c46d5ae38c1adc8bd5d2202829a6e00eb
Lameck Santos <lameck2@hotmail.com>
2024-11-14 05:14:21 -0300

    feat: Add 'comma ok idiom'

--- learning-go/ch3/maps.go
+++ learning-go/ch3/maps.go
@@ -15,4 +15,17 @@ func maps() {
 	totalWins["Lions"]++
 
 	fmt.Println(totalWins["Lions"])
+
+	m := map[string]int{
+		"hello": 5,
+		"world": 0,
+	}
+	v, ok := m["hello"]
+	fmt.Println(v, ok)
+
+	v, ok2 := m["world"]
+	fmt.Println(v, ok2)
+
+	v, ok3 := m["goodbye"]
+	fmt.Println(v, ok3)
 }
