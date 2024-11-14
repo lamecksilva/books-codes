4441213bf766f521f470fa44f236cafc6d9be7ef
Lameck Santos <lameck2@hotmail.com>
2024-11-14 05:10:27 -0300

    feat: Using maps, strings, slices, arrays

---
+++ learning-go/ch3/strings.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func strings() {
+	var s string = "Hello There"
+	var b byte = s[6]
+	fmt.Println(s)
+	fmt.Println(b)
+}
---
+++ learning-go/ch3/using_slices.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	xSlice := []int{1, 2, 3, 4}
+	xArray := [4]int(xSlice)
+	smallArray := [2]int(xSlice)
+	xSlice[0] = 10
+
+	fmt.Println(xSlice)
+	fmt.Println(xArray)
+	fmt.Println(smallArray)
+
+	strings()
+	maps()
+}
